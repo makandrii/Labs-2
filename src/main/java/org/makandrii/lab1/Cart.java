6b1f4950d52d9221cfd99be9c139f54a2827f36c
makandrii <makarov.andrii.10@gmail.com>
2024-02-19 14:41:27 +0200

    implement Iterable for Cart and add function of creating Order

--- src/main/java/org/makandrii/lab1/Cart.java
+++ src/main/java/org/makandrii/lab1/Cart.java
@@ -1,9 +1,11 @@
 package org.makandrii.lab1;
 
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
+import java.util.Spliterator;
 
-public class Cart {
+public class Cart implements Iterable<Map.Entry<Product, Integer>> {
     private final Map<Product, Integer> cart;
 
     protected Cart() {
@@ -12,5 +14,19 @@ public class Cart {
 
     protected void add(Product product, Integer amount) {
         cart.merge(product, amount, Integer::sum);
+    }
+
+    protected Order makeOrder() {
+        return new Order(cart);
+    }
+
+    @Override
+    public Iterator<Map.Entry<Product, Integer>> iterator() {
+        return cart.entrySet().iterator();
+    }
+
+    @Override
+    public Spliterator<Map.Entry<Product, Integer>> spliterator() {
+        return cart.entrySet().spliterator();
     }
 }
--- src/main/java/org/makandrii/lab1/Order.java
+++ src/main/java/org/makandrii/lab1/Order.java
@@ -8,7 +8,7 @@ public class Order {
     private final double totalPrice;
     private final LocalDate date;
 
-    public Order(Map<Product, Integer> products) {
+    protected Order(Map<Product, Integer> products) {
         this.products = products;
         this.totalPrice = calculateTotalPrice();
         this.date = LocalDate.now();
